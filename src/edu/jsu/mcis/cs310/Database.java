e2d90d950f92c90803b02db0bbb19bba33508b52
Ahmed <Ahmed@192.168.1.4>
2022-09-30 20:22:35 +0200

    build the missing methods

--- src/edu/jsu/mcis/cs310/Database.java
+++ src/edu/jsu/mcis/cs310/Database.java
@@ -1,175 +1,211 @@
 package edu.jsu.mcis.cs310;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
 public class Database {
-    
+
     private final Connection connection;
-    
+
     private final int TERMID_SP22 = 1;
-    
+
     /* CONSTRUCTOR */
-
     public Database(String username, String password, String address) {
-        
         this.connection = openConnection(username, password, address);
-        
     }
-    
-    /* PUBLIC METHODS */
 
+    /* PUBLIC METHODS */
     public String getSectionsAsJSON(int termid, String subjectid, String num) {
-        
+
         String result = null;
-        
         // INSERT YOUR CODE HERE
-        
+        String query = "select * from section where termid=? and  subjectid=? and num=?";
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1, termid);
+            pstmt.setString(2, subjectid);
+            pstmt.setString(3, num);
+            pstmt.execute();
+            ResultSet resultset = pstmt.getResultSet();
+            result = getResultSetAsJSON(resultset);
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return result;
-        
     }
-    
+
     public int register(int studentid, int termid, int crn) {
-        
+
         int result = 0;
-        
         // INSERT YOUR CODE HERE
-        
+        String query = "insert into registration values(?,?,?)";
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(query);
+            ps.setInt(1, studentid);
+            ps.setInt(2, termid);
+            ps.setInt(3, crn);
+            result = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return result;
-        
     }
 
     public int drop(int studentid, int termid, int crn) {
-        
+
         int result = 0;
-        
+
         // INSERT YOUR CODE HERE
-        
-        return result;
-        
-    }
-    
-    public int withdraw(int studentid, int termid) {
-        
-        int result = 0;
-        
-        // INSERT YOUR CODE HERE
-        
-        return result;
-        
-    }
-    
-    public String getScheduleAsJSON(int studentid, int termid) {
-        
-        String result = null;
-        
-        // INSERT YOUR CODE HERE
-        
-        return result;
-        
-    }
-    
-    public int getStudentId(String username) {
-        
-        int id = 0;
-        
+        String query = "delete from registration where studentid=? and termid=? and crn=?";
         try {
-        
+            PreparedStatement ps = this.connection.prepareStatement(query);
+            ps.setInt(1, studentid);
+            ps.setInt(2, termid);
+            ps.setInt(3, crn);
+            result = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    public int withdraw(int studentid, int termid) {
+
+        int result = 0;
+        String query = "delete from registration where studentid=? and termid=?";
+        try {
+            PreparedStatement ps = this.connection.prepareStatement(query);
+            ps.setInt(1, studentid);
+            ps.setInt(2, termid);
+            result = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // INSERT YOUR CODE HERE
+        return result;
+
+    }
+
+    public String getScheduleAsJSON(int studentid, int termid) {
+// list 
+        String result = null;
+
+//        String query = "SELECT section.* FROM registration join section on registration.crn=section.crn and  where section.termid=? and registration.studentid=?  ";
+        String query = "SELECT r.studentid, r.termid, s.scheduletypeid, s.instructor, s.num, s.start, s.days, s.section, s.end, s.where, s.crn, s.subjectid FROM registration AS r, section AS s, term AS t WHERE r.studentid=? AND r.termid=? AND r.crn=s.crn AND s.termid=t.id";
+
+        PreparedStatement pstmt;
+        try {
+            pstmt = connection.prepareStatement(query);
+            pstmt.setInt(1, termid);
+            pstmt.setInt(2, studentid);
+            pstmt.execute();
+            ResultSet resultset = pstmt.getResultSet();
+            result = getResultSetAsJSON(resultset);
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    public int getStudentId(String username) {
+
+        int id = 0;
+
+        try {
             String query = "SELECT * FROM student WHERE username = ?";
             PreparedStatement pstmt = connection.prepareStatement(query);
             pstmt.setString(1, username);
-            
             boolean hasresults = pstmt.execute();
-            
-            if ( hasresults ) {
-                
+
+            if (hasresults) {
+
                 ResultSet resultset = pstmt.getResultSet();
-                
-                if (resultset.next())
-                    
+
+                if (resultset.next()) {
                     id = resultset.getInt("id");
-                
+                }
+
             }
-            
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (Exception e) { e.printStackTrace(); }
-        
+
         return id;
-        
     }
-    
+
     public boolean isConnected() {
 
         boolean result = false;
-        
+
         try {
-            
-            if ( !(connection == null) )
-                
+
+            if (!(connection == null)) {
                 result = !(connection.isClosed());
-            
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (Exception e) { e.printStackTrace(); }
-        
+
         return result;
-        
+
     }
-    
+
     /* PRIVATE METHODS */
-
     private Connection openConnection(String u, String p, String a) {
-        
-        Connection c = null;
-        
-        if (a.equals("") || u.equals("") || p.equals(""))
-            
-            System.err.println("*** ERROR: MUST SPECIFY ADDRESS/USERNAME/PASSWORD BEFORE OPENING DATABASE CONNECTION ***");
-        
-        else {
-        
-            try {
 
+        Connection c = null;
+
+        if (a.equals("") || u.equals("") || p.equals("")) {
+            System.err.println("*** ERROR: MUST SPECIFY ADDRESS/USERNAME/PASSWORD BEFORE OPENING DATABASE CONNECTION ***");
+        } else {
+            try {
                 String url = "jdbc:mysql://" + a + "/jsu_sp22_v1?autoReconnect=true&useSSL=false&zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=America/Chicago";
                 // System.err.println("Connecting to " + url + " ...");
-
                 c = DriverManager.getConnection(url, u, p);
 
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            catch (Exception e) { e.printStackTrace(); }
-        
         }
-        
         return c;
-        
+
     }
-    
+
     private String getResultSetAsJSON(ResultSet resultset) {
-        
+
         String result;
-        
+
         /* Create JSON Containers */
-        
         JSONArray json = new JSONArray();
         JSONArray keys = new JSONArray();
-        
+        // INSERT YOUR CODE HERE
+
         try {
-            
+
             /* Get Metadata */
-        
             ResultSetMetaData metadata = resultset.getMetaData();
             int columnCount = metadata.getColumnCount();
-            
-            // INSERT YOUR CODE HERE
-        
+
+            while (resultset.next()) {
+                JSONObject obj = new JSONObject();
+                for (int i = 0; i < columnCount; i++) {
+                    obj.put(metadata.getColumnLabel(i + 1).toLowerCase(), resultset.getObject(i + 1).toString());
+                }
+                json.add(obj);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (Exception e) { e.printStackTrace(); }
-        
+
         /* Encode JSON Data and Return */
-        
         result = JSONValue.toJSONString(json);
         return result;
-        
     }
-    
 }
--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -5,13 +5,10 @@ import org.json.simple.*;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         Database db = new Database("cs310_p2_user", "P2!user", "localhost");
-        
-        if (db.isConnected())
-            
-            System.err.println("Connected Successfully!");
-        
+        if (db.isConnected()) {
+            System.out.println("Connected");
+        }
     }
-    
 }
